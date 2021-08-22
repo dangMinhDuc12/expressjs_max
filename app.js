37837e5ade0321e47a661e17fab2a67e8f83b216
Duc Dang <dangminhduca3@gmail.com>
2021-08-22 11:54:01 +0700

    add template engine ejs

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const express = require('express')
 const app = express()
 
 //Routes
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const path = require("path");
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin',adminRoutes)
+app.use('/admin',adminData.router)
 app.use(shopRoutes)
 
 //404 not found page
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {pageTitle: 'Page not found'})
 })
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,20 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const rootDir = require('../ulti/path')
+const adminData = require('./admin')
 
 //  / => GET
 router.get('/',(req, res, next) => {
     //sendFile: gửi 1 file đến đường dẫn bên trong hàm, dirname: folder hiện tại
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    })
 } )
 
 module.exports = router
